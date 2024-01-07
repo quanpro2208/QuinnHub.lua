3c251a710dbb7577c49238ff5c4b7423ee728bb4
quanpro2208 <92915634+quanpro2208@users.noreply.github.com>
2024-01-07 11:28:27 +0700

    QuinnHub.lua

---
+++ QuinnHub.lua
@@ -0,0 +1,61 @@
+getgenv().Setting = {
+    ["Team"] = "Pirates",
+    ["Webhook"] = {
+        ["Enable"] = false, 
+        ["Url"] = ""
+    },
+    ["Skip"] = {
+        ["Race V4"] = true,
+        ["Fruit"] = { 
+            "Leopard-Leopard",
+            "Venom-Venom",
+            "Dough-Dough",
+            "Portal-Portal"
+        }
+    },
+    ["Chat"] = {
+        ["Enabled"] = false,
+        ["Content"] = {""},
+    },
+    ["Misc"] = {
+        ["Lock Bounty"] = {0, 30000000},
+        ["Hide If Low Health"] = true,
+        ["Hide Health"] = {4000,5000},
+        ["Lock Camera"] = true,
+        ["FPS Boost"] = false,
+        ["White Screen"] = false,
+        ["Bypass TP"] = true, 
+        ["Spam All Skill On V4"] = true, 
+        ["Random Fruit & Store"] = false,
+        ["Random Suprise"] = true
+    },
+    ["Melee"] = {
+        ["Enable"] = true,
+        ["Delay"] = 3,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["C"] = {["Enable"] = true, ["Hold Time"] = 0}
+    },
+    ["Fruit"] = {
+        ["Enable"] = false,
+        ["Delay"] = 2,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["C"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["V"] = {["Enable"] = false, ["Hold Time"] = 1.25},
+        ["F"] = {["Enable"] = true, ["Hold Time"] = 0}
+    },
+    ["Sword"] = {
+        ["Enable"] = true,
+        ["Delay"] = 0.1,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
+    },
+    ["Gun"] = {
+        ["Enable"] = true,
+        ["Delay"] = 0.1,
+        ["Z"] = {["Enable"] = true, ["Hold Time"] = 0},
+        ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vuquocoai123/lua/main/AutoBountyV2Beta"))()
